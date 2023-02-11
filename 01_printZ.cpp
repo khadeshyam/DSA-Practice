105a6f962cf050dd508dd3911db6c8524e282635
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-11 14:31:41 +0530

    print Z using nested for loops

--- 01_printZ.cpp
+++ 01_printZ.cpp
@@ -7,5 +7,25 @@ int main(int argc, char** agrc) {
   cout<<"  *  "<<endl; 
   cout<<" *   "<<endl; 
   cout<<"*****"<<endl; 
-
+  
+  cout<<"\nUsing Loops"<<endl;
+  
+  for(int i=0;i<5;i++){
+    if(i == 0 || i == 4){
+        for(int j = 0;j<5;j++){
+           cout<<"*"; 
+        }cout<<endl;  
+    }
+    else{
+         for(int j = 0;j<5;j++){
+              if( j == 4-i ){
+                    cout<<"*"; 
+              }
+              else{
+                cout<<" ";
+              }
+         }cout<<endl;  
+    } 
+  }
+ 
 }

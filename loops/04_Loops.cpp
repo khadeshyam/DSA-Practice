aa22e0482def1fa6c6767c72cedbb093f63b3685
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-11 15:44:00 +0530

    learn loops in cpp

---
+++ loops/04_Loops.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while (i <= 9)
+    {
+        cout << i << endl;
+        //i = i + 1;
+        i++;
+    }
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << endl;
+    }
+
+    cout << "Done. " << endl;
+
+    string name;
+    cin>>name;
+
+    for(int i=0;i<9;i++){
+        cout<<name<<endl;
+    }
+
+    return 0;
+}

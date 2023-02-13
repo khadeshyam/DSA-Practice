ceadc13de6b3c07d3a2601004eb44e9022affc46
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-13 16:04:15 +0530

    learn using namespace std in cpp

---
+++ namespaces/02_using_namespace_std.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char name[100];
+    cout << "enter your name" << endl;
+    cin >> name;
+    cout << "enter your age" << endl;
+    int age;
+    cin >> age;
+    cout << "Your name is " << name << " and your age is " << age << endl;
+}

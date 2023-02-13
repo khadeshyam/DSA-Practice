312a51b06e226d06c6f2577a611c46d705b4e11b
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-13 16:02:31 +0530

    learn using std::cout/cin/endl in cpp

---
+++ namespaces/01_using_std.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(){
+  char name[100];
+  cout<<"enter your name"<<endl;
+  cin >> name;
+ cout<<"enter your age"<<endl;
+  int age;
+  cin>>age;
+  cout<<"Your name is "<<name<<" and your age is "<<age<<endl;
+}

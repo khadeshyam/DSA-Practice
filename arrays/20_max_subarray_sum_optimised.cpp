c36b4e9c13f48fbc44d50ea726a6e4f669777987
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-23 17:54:56 +0530

    learn kadanes algo to find max subarray sum

---
+++ arrays/20_max_subarray_sum_optimised.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int currSum = nums[0];
+       int maxSum = nums[0];
+
+       for(int i = 1 ; i < nums.size() ; i++){
+           if(currSum >= 0){
+              currSum += nums[i];
+           }
+           else{
+              currSum = nums[i];
+           }
+           maxSum = max(maxSum,currSum);
+       }
+       return maxSum;
+    }
+};
